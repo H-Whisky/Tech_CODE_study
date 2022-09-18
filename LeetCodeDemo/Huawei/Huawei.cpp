d1f6beddddfff2fb44e96138857f5f2ad2fa4ac4
XiaoxinPro16-Win11-HWhisky <2804127361@qq.com>
2022-09-18 15:27:15 +0800

    update code 23

--- LeetCodeDemo/Huawei/Huawei.cpp
+++ LeetCodeDemo/Huawei/Huawei.cpp
@@ -13,6 +13,15 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr){}
+	ListNode(int x) : val(x), next(nullptr){}
+	ListNode(int x, ListNode *next) : val(x), next(next){}
+};
+
 // 一、递归问题
 #if 0
 class Solution_70 {
@@ -47,7 +56,7 @@ void main() {
 
 #endif
 
-#if 1
+#if 0
 class Solution_112 {
 public:
 	//struct TreeNode {
@@ -58,6 +67,8 @@ public:
 	//	TreeNode(int x) : val(x),left(nullptr), right(nullptr){}
 	//	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right){}
 	//};
+	
+	//深度优先遍历
 	bool hasPathSum_0(TreeNode* root, int targetSum) {
 		if (root == nullptr) {
 			return false;
@@ -69,6 +80,7 @@ public:
 			hasPathSum_0(root->right, targetSum - root->val);
 	}
 
+	//广度优先遍历
 	bool hasPathSum_1(TreeNode* root, int targetSum) {
 		if (root == nullptr) {
 			return false;
@@ -116,6 +128,101 @@ int main() {
 }
 
 #endif // 1
+
+#if 0
+class Solution_509 {
+public:
+	int fib(int n) {
+		if (n == 0) {
+			return 0;
+		}
+
+		if (n == 1) {
+			return 1;
+		}
+
+		return (fib(n - 1) + fib(n - 2));
+	}
+};
+
+void main() {
+	Solution_509* sol_509 = new Solution_509();
+	int n;
+	std::cin >> n;
+	std::cout << sol_509->fib(n) << std::endl;
+}
+
+#endif
+
+// 二、分治问题
+#if 1
+class Solution_23 {
+public:
+	// 暴力解法
+	ListNode* mergeKLists_0(vector<ListNode*>& lists) {
+		vector<int> num;
+
+		// 取出所有的节点的值
+		for (auto a : lists) {
+			while (a != NULL) {
+				num.push_back(a->val);
+				a = a->next;
+			}
+		}
+
+		// 排序
+		sort(num.begin(), num.end());
+
+		// 如果为空直接返回
+		if (num.size() == 0) {
+			return NULL;
+		}
+
+		ListNode* res = new ListNode(0);
+		ListNode* p = res;
+
+		//创建新链表
+		for (auto a : num) {
+			p->next = new ListNode(a);
+			p = p->next;
+		}
+		return res->next;
+	}
+
+	// 顺序合并
+	ListNode* mergeTwoLists_1(ListNode* a, ListNode* b) {
+		if ((!a) || (!b)) {
+			return a ? a : b;
+		}
+		ListNode head, *tail = &head, *aPtr = a, *bPtr = b;
+		while (aPtr && bPtr) {
+			if (aPtr->val < bPtr->val){
+				tail->next = aPtr;
+				aPtr = aPtr->next;
+			}
+			else {
+				tail->next = bPtr;
+				bPtr = bPtr->next;
+			}
+			tail = tail->next;
+		}
+		tail->next = (aPtr ? aPtr : bPtr);
+		return head.next;
+	}
+
+	ListNode* mergeKlists(vector<ListNode*>& lists) {
+		ListNode* ans = nullptr;
+		for (size_t i = 0; i < lists.size(); ++i) {
+			ans = mergeTwoLists_1(ans, lists[i]);
+		}
+		return ans;
+	}
+
+};
+
+
+
+#endif
 
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
