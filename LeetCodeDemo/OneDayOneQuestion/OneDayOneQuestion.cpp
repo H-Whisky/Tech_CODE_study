8e24310bb6170be4d50f385e1e04d107ab046dfc
XiaoxinPro16-Win11-HWhisky <2804127361@qq.com>
2022-09-28 14:36:05 +0800

    add foler OneDayOneQuestion

---
+++ LeetCodeDemo/OneDayOneQuestion/OneDayOneQuestion.cpp
@@ -0,0 +1,86 @@
+﻿// OneDayOneQuestion.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+// 每日一题
+#if 1
+class Solution_day {
+public:
+	/*
+		20220927
+		给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+		示例 1：
+		输入 : s1 = "abc", s2 = "bca"
+		输出 : true
+		示例 2：
+		输入 : s1 = "abc", s2 = "bad"
+		输出 : false
+	*/
+
+	// 排序
+	bool CheckPermutation_20220927_0(string s1, string s2) {
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+
+		sort(s1.begin(), s1.end());
+		sort(s2.begin(), s2.end());
+
+		return s1 == s2;
+	}
+
+	// 哈希表
+	bool CheckPermutation_20220927_1(string s1, string s2) {
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+		vector<int> ans(128, 0);
+
+		for (auto it : s1) {
+			ans[it]++;
+
+		}
+
+		for (auto it : s2) {
+			ans[it]--;
+			if (ans[it] < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+};
+
+void main() {
+	string s1 = { "abcq" };
+	string s2 = { "bcaq" };
+	Solution_day* sol_day = new Solution_day;
+	cout << sol_day->CheckPermutation_20220927_0(s1, s2) << endl;
+	cout << sol_day->CheckPermutation_20220927_1(s1, s2) << endl;
+
+}
+#endif
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
