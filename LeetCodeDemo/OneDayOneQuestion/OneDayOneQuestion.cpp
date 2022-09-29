902bca08b4fa06178db49933bcc3683a0d442dc3
XiaoxinPro16-Win11-HWhisky <2804127361@qq.com>
2022-09-29 16:54:47 +0800

    update sol_day_0928

--- LeetCodeDemo/OneDayOneQuestion/OneDayOneQuestion.cpp
+++ LeetCodeDemo/OneDayOneQuestion/OneDayOneQuestion.cpp
@@ -75,7 +75,7 @@ void main() {
 }
 #endif
 
-#if 1
+#if 0
 /*
 有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。
 注意，不是必须有这些素因子，而是必须不包含其他的素因子。
@@ -114,12 +114,32 @@ public:
 	}
 
 	// 动规
+	int getKthMagicNumber_1(int k) {
+		vector<int> dp(k + 1);
+		dp[1] = 1;
+		int p3 = 1, p5 = 1, p7 = 1;
+		for (int i = 2; i <= k; ++i) {
+			int num3 = dp[p3] * 3, num5 = dp[p5] * 5, num7 = dp[p7] * 7;
+			dp[i] = min(min(num3, num5), num7);
+			if (dp[i] == num3) {
+				p3++;
+			}
+			if (dp[i] == num5) {
+				p5++;
+			}
+			if (dp[i] == num7) {
+				p7++;
+			}
+		}
+		return dp[k];
+	}
 
 };
 
 void main() {
 	Solution_day_20220928* sol_0928 = new Solution_day_20220928;
-	cout << sol_0928->getKthMagicNumber_0(9);
+	cout << sol_0928->getKthMagicNumber_0(5)<<endl;
+	cout << sol_0928->getKthMagicNumber_1(5)<<endl;
 }
 #endif
 
