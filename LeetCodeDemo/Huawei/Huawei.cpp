31cdc0e8105e41a57440b07eef06aa219d3900bf
XiaoxinPro16-Win11-HWhisky <2804127361@qq.com>
2022-10-10 18:22:20 +0800

    add sol_210

--- LeetCodeDemo/Huawei/Huawei.cpp
+++ LeetCodeDemo/Huawei/Huawei.cpp
@@ -1472,7 +1472,7 @@ void main() {
 }
 #endif
 
-#if 1
+#if 0
 class Solution_122 {
 public:
 	// 贪心
@@ -1504,6 +1504,78 @@ void main() {
 	//cout << sol->maxProfit_0(prices) << endl;
 	cout << sol->maxProfit_1(prices) << endl;
 
+}
+#endif
+
+#if 1
+class Solution_210 {
+private:
+	// 存储有向图
+	vector<vector<int>> edges;
+	// 标记每个节点的状态：0=未搜索 1=搜索中 2=已完成
+	vector<int> visited;
+	// 用数组来模拟栈，下标0为栈底，n-1为栈顶
+	vector<int> result;
+	// 判断有向图中是否有环
+	bool valid = true;
+public:
+	void dfs(int u) {
+		// 将节点标记为[搜索中]
+		visited[u] = 1;
+		// 搜索其相邻节点
+		// 只要发现有环，立刻停止搜索
+		for (int v : edges[u]) {
+			// 如果[未搜索]那么搜索相邻节点
+			if (visited[v] == 0) {
+				dfs(v);
+				if (!valid) {
+					return;
+				}
+			}
+			// 如果 [搜索中]说明找到了环
+			else if (visited[v] == 1) {
+				valid = false;
+				return;
+			}
+		}
+		// 将节点标记为[已完成]
+		visited[u] = 2;
+		// 将节点入栈
+		result.push_back(u);
+	}
+	// DFS
+	vector<int> findOrder_0(int numCourses, vector<vector<int>>& prerequisites) {
+		edges.resize(numCourses);
+		visited.resize(numCourses);
+		for (const auto& info : prerequisites) {
+			edges[info[1]].push_back(info[0]);
+		}
+
+		// 每次挑选一个[未搜索]的节点，开始进行深度优先搜索
+		for (int i = 0; i < numCourses && valid; ++i) {
+			if (!visited[i]) {
+				dfs(i);
+			}
+		}
+
+		if (!valid) {
+			return {};
+		}
+
+		// 如果没有环，那么就拓扑排序
+		// 注意下标0为栈底，因此需要将数组反序输出
+		reverse(result.begin(), result.end());
+		return result;
+	}
+};
+void main() {
+	Solution_210* sol = new Solution_210;
+	vector<vector<int>> prerequisites = { {1,0} };
+	int numCourses = 2;
+	vector<int> res = sol->findOrder_0(numCourses, prerequisites);
+	for (int i = 0; i < res.size(); ++i) {
+		cout << res[i] << " ";
+	}
 }
 #endif
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
