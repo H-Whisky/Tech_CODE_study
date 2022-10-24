d910d36c968e16ea21d7cdbf7bfaef8f3f416c70
H-Whisky <2804127361@qq.com>
2022-10-24 14:17:34 +0800

    add sol_915_day_20221024, Happy Day1024!

--- LeetCodeDemo/OneDayOneQuestion/OneDayOneQuestion.cpp
+++ LeetCodeDemo/OneDayOneQuestion/OneDayOneQuestion.cpp
@@ -858,7 +858,7 @@ void main() {
 }
 #endif
 
-#if 1
+#if 0
 /*
                 -  
       - 
@@ -907,4 +907,49 @@ void main() {
 	cout << sol->next(85)<< endl;
 
 }
+#endif
+
+#if 1
+class Solution_915_day_20221024 {
+public:
+	// 两次遍历
+	int partitionDisjoint_0(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> minRight(n);
+		minRight[n - 1] = nums[n - 1];
+		for (int i = n - 2; i >= 0; i--) {
+			minRight[i] = min(nums[i], minRight[i + 1]);
+		}
+		int maxLeft = 0;
+		for (int i = 0; i < n - 1; i++) {
+			maxLeft = max(maxLeft, nums[i]);
+			if (maxLeft <= minRight[i + 1]) {
+				return i + 1;
+			}
+		}
+		return n - 1;
+	}
+
+	// 一次遍历
+	int partitionDisjoint_1(vector<int>& nums) {
+		int n = nums.size();
+		int leftMax = nums[0], leftPos = 0, curMax = nums[0];
+		for (int i = 1; i < n - 1; i++) {
+			curMax = max(curMax, nums[i]);
+			if (nums[i] < leftMax) {
+				leftMax = curMax;
+				leftPos = i;
+			}
+		}
+		return leftPos + 1;
+	}
+};
+
+void main() {
+	Solution_915_day_20221024* sol = new Solution_915_day_20221024;
+	vector<int> nums = { 5,0,3,8,6 };
+	//cout << sol->partitionDisjoint_0(nums) << endl;
+	cout << sol->partitionDisjoint_1(nums) << endl;
+}
+
 #endif
