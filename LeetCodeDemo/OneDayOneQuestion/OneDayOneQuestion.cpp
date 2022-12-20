cf58650e896e058ba390558081013a3e00a671e5
H-Whisky <2804127361@qq.com>
2022-12-20 16:49:02 +0800

    add sol_1971_day_20221219

--- LeetCodeDemo/OneDayOneQuestion/OneDayOneQuestion.cpp
+++ LeetCodeDemo/OneDayOneQuestion/OneDayOneQuestion.cpp
@@ -1332,7 +1332,7 @@ void main() {
 }
 #endif
 
-#if 1
+#if 0
 class Sol_754_day_20221104 {
 public:
 
@@ -1351,5 +1351,72 @@ public:
 void main() {
 	Sol_754_day_20221104* sol = new Sol_754_day_20221104;
 	cout << sol->reachNumber(2) << endl;
+}
+#endif
+
+#if 1
+class Sol_1971_day_20221219 {
+public:
+	// 广度优先搜索
+	bool validPath_0(int n, vector<vector<int>>& edges, int source, int destination) {
+		vector<vector<int>> adj(n);
+		for (auto&& edge : edges) {
+			int x = edge[0], y = edge[1];
+			adj[x].emplace_back(y);
+			adj[y].emplace_back(x);
+		}
+		vector<bool> visited(n, false);
+		queue<int> qu;
+		qu.emplace(source);
+		visited[source] = true;
+		while (!qu.empty()) {
+			int vertex = qu.front();
+			qu.pop();
+			if (vertex == destination) {
+				break;
+			}
+			for (int next : adj[vertex]) {
+				if (!visited[next]) {
+					qu.emplace(next);
+					visited[next] = true;
+				}
+			}
+		}
+		return visited[destination];
+	}
+
+	// 深度优先搜索
+	bool validPath_1(int n, vector<vector<int>>& edges, int source, int destination) {
+		vector<vector<int>> adj(n);
+		for (auto& edge : edges) {
+			int  x = edge[0], y = edge[1];
+			adj[x].emplace_back(y);
+			adj[y].emplace_back(x);
+		}
+		vector<bool> visited(n, false);
+		return dfs(source, destination, adj, visited);
+	}
+
+	bool dfs(int source, int destination, vector<vector<int>>& adj, vector<bool>& visited) {
+		if (source == destination) {
+			return true;
+		}
+		visited[source] = true;
+		for (int next : adj[source]) {
+			if (!visited[next] && dfs(next, destination, adj, visited)) {
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+void main() {
+	Sol_1971_day_20221219* sol = new Sol_1971_day_20221219;
+	vector<vector<int>> edges = { {0,1},{1,2},{2,0} };
+	int n=3, source = 0, destination = 2;
+	cout << sol->validPath_0(n, edges, source, destination) << endl;
+	cout << sol->validPath_1(n, edges, source, destination) << endl;
+	
 }
 #endif
