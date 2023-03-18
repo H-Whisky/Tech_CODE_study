1047103f54d2e4a4494485aef144929913354114
H-Whisky <2804127361@qq.com>
2023-03-18 14:24:43 +0800

    update CplusplusPrimer.cpp

--- CplusplusBookDemo/CplusplusPrimer/CplusplusPrimer.cpp
+++ CplusplusBookDemo/CplusplusPrimer/CplusplusPrimer.cpp
@@ -123,7 +123,20 @@ int main()
     cout << "请输入若干销售记录:" << endl;
     if (cin >> trans1) {
         while (cin >> trans2) {
-
+            if (compareIsbn(trans1, trans2)) { // ISBN相同
+                num++;
+            }
+            else { // ISBN不同
+                cout << trans1.isbn() << "共有" << num << "条销售记录" << endl;
+                trans1 = trans2;
+                num = 1;
+            }
+            cout << trans1.isbn() << "共有" << num << "条销售记录" << endl;
         }
     }
+    else {
+        cout << "没有数据" << endl;
+        return -1;
+    }
+    return 0;
 }
