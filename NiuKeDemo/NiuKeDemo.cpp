9f7069819ff442352317e2a0d98f75102c52d795
H-Whisky <2804127361@qq.com>
2023-03-22 17:21:18 +0800

    add HJ36

--- NiuKeDemo/NiuKeDemo.cpp
+++ NiuKeDemo/NiuKeDemo.cpp
@@ -780,7 +780,63 @@ int main() {
 
 class HUAWEI_HJ36 {
 public:
-
+	void Solution_1() {
+		string sKey, sStr;
+		while (getline(cin, sKey) && getline(cin, sStr)) {
+			// key中大写变为小写
+			for (int i = 0; i < sKey.size(); i++) {
+				if (isupper(sKey[i])) {
+					sKey[i] += 32;
+				}
+			}
+			// set记录有多少个元素
+			unordered_set<char> set;
+			for (int i = 0; i < sKey.size(); i++) {
+				set.insert(sKey[i]);
+			}
+			// 输入不重样字符的key
+			vector<char> arr(26);
+			int j = 0;
+			for (int i = 0; i < sKey.size(); i++) {
+				int flag = 0;
+				for (char c : arr) {
+					if (c == sKey[i]) {
+						flag = 1;
+					}
+				}
+				if (flag == 1) {
+					arr[j++] = sKey[i];
+				}
+			}
+			// 输入剩余字符
+			for (int i = 0; i < 26; i++) {
+				if (set.count('a' + i) == 0) {
+					arr[j++] = 'a' + i;
+				}
+			}
+			// 输出
+			string ans;
+			for (int i = 0; i < sStr.size(); i++) {
+				if (isupper(sStr[i])) {
+					ans += (arr[sStr[i] - 'A'] - 'a' + 'A');
+					continue;
+				}
+				else if (islower(sStr[i])) {
+					ans += arr[sStr[i] - 'a'];
+					continue;
+				}
+				else {
+					ans += sStr[i];
+				}
+			}
+			cout << ans << endl;
+		}
+	}
 };
 
+int main() {
+	HUAWEI_HJ36* hj36 = new HUAWEI_HJ36;
+	hj36->Solution_1();
+	return 0;
+}
 #endif
