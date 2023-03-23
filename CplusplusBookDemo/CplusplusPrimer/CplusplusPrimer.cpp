aace2f002bba809529522b2174fed881b010e012
HWhisky_hp <2804127361@qq.com>
2023-03-23 23:30:51 +0800

    update CplusplusPrimer.cpp

--- CplusplusBookDemo/CplusplusPrimer/CplusplusPrimer.cpp
+++ CplusplusBookDemo/CplusplusPrimer/CplusplusPrimer.cpp
@@ -149,7 +149,7 @@ int main()
 
 using namespace std;
 
-int main() { // 使用getline一次读入一整行，遇回车结束
+int Solution_1() { // 使用getline一次读入一整行，遇回车结束
     string line;
     // 循环读取，每次读入一整行，直至文件结束或遇到异常输入
     cout << "请输入您的字符串，可以包含空格：" << endl;
@@ -158,5 +158,49 @@ int main() { // 使用getline一次读入一整行，遇回车结束
     }
     return 0;
 
+}
+
+int Solution_2() { //  使用cin一次读入一个词，遇到空白结束
+    string word;
+    // 循环读取，每次读入一个词，直至文件结束或遇到异常输入
+    cout << "请输入您的单词，不可以包含空格：" << endl;
+    while (cin >> word) {
+        cout << word << endl; // 为了便于观察，输出每个单词后换行
+    }
+    return 0;
+}
+
+int Solution_3() {
+    string word, line;
+    cout << "请输入读取字符串的方式：1表示逐词读取，2表示整行读取" << endl;
+    char ch;
+    cin >> ch;
+    if (ch == '1') {
+        cout << "请输入字符串： Welcome to C++ family!" << endl;
+        cin >> word;
+        cout << "系统读取的有效字符串是：" << endl;
+        cout << word << endl;
+        return 0;
+    }
+    // 清空输入缓存区
+    cin.clear();
+    cin.sync();
+    if (ch == '2') {
+        cout << "请输入字符串： Welcome to C++ family!" << endl;
+        getline(cin, line);
+        cout << "系统读取的有效字符串是：" << endl;
+        cout << line << endl;
+        return 0;
+    }
+    cout << "您的输入有误！";
+
+    return -1;
+}
+
+int main() {
+    //Solution_1();
+    //Solution_2();
+    Solution_3();
+    return 0;
 }
 #endif
